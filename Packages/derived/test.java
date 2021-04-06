807381b285b2fadd93f3ffc9a77d762b15e80921
Akash Chouhan <akash.c1500@gmail.com>
2021-04-06 20:57:57 +0530

    package test commit

---
+++ Packages/derived/test.java
@@ -0,0 +1,7 @@
+import general.Base;
+
+public class test {
+    public static void main(String[] args) {
+        new Base().show();
+    }
+}
---
+++ Packages/general/Base.java
@@ -0,0 +1,7 @@
+package general;
+
+public class Base {
+    public void show() {
+        System.out.println("Hello from Base Class");
+    }
+}
